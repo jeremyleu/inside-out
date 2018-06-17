8bf3bce4391d9bce22f97294998c0e37a6c00d6e
Jeremy Leu <jeremyleu@yahoo.com>
2018-06-16 18:34:58 -0700

    add google site verification

--- src/html.js
+++ src/html.js
@@ -29,6 +29,7 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <meta name="google-site-verification" content="jb5EUTr4Z0ELbw_ovUD-fs_Phzo1oSdrWPH8TXfIIl0" />
           <link rel="stylesheet" href="https://use.typekit.net/mpt2tzt.css" />
           {this.props.headComponents}
           {css}
